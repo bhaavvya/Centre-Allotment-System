ebf03b4c4b0fce3b656714f7aeea55674b1fbb54
Bhavya <110487270+bhaavvya@users.noreply.github.com>
2023-01-07 19:22:01 +0530

    Add files via upload

---
+++ ExamSeatingProject.java
@@ -0,0 +1,8 @@
+import exam_seating.*;
+public class ExamSeatingProject{
+    public static void main(String args[]){
+        System.out.println("above");
+        new Main();
+        System.out.println("below");
+    }
+}
